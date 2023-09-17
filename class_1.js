f1ea4aafd423d64617458a9a3da419b512fa7cc8
Hitesh Bhawsar <hiteshbhawsar@Togopools-MacBook-Pro.local>
2023-09-17 14:08:23 +0530

    class 1

---
+++ class_1.js
@@ -0,0 +1,66 @@
+// // Backend Development
+
+// // --> git , github
+// // --> apis , https method, gateways , server
+
+
+// // --> javascript
+// // --> nodejs
+// // --> mysql
+
+
+
+// javascript
+
+
+// synchronous , Asynchronous
+// single thread , multithread
+
+
+
+// javascript - synchonous , single threaded language
+
+// console.log(a);
+// console.log(b);
+
+
+
+// --> datatype, number , string , bigint , boolean , symbol, undefined, null
+
+
+// -> variables -- var , let , const (difference)
+
+// --> function --> 
+
+// -- hoisting
+
+// -- 
+
+// // object oriented programming --> class
+// // functional programming. --> functions
+
+// // functional programming
+
+// callstack, event loop , 
+
+
+
+// --> working
+// -- hoisting
+// -- functions
+// -- callback
+// -- timer fucntions
+// -- scope chaining
+// -- clousure
+// -- promises
+// -- async await
+// -- module
+
+
+// --- Nodejs (javascript run time environment)
+
+// -- npm
+// -- packages
+// -- express
+// -- server
+// -- apis
