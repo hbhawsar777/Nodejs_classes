343ba66d34a1cf2d7ec5f4c28ac1f3b2aec1807e
Hitesh Bhawsar <hiteshbhawsar@Togopools-MacBook-Pro.local>
2023-09-17 14:46:45 +0530

    class-2 added

---
+++ class_2.js
@@ -0,0 +1,95 @@
+
+// Functions in JavaScript
+
+// -- block of statements , name
+
+// function defination , function declaration , function call
+
+// arguments , parameters
+
+// return -- function can return the values // function by default returns undefined
+
+//--------------------------------------------------
+
+// function Types
+
+/*
+1. normal function , function statement
+2. anonymouse function 
+3. Arrow function
+4. Function Expression
+*/
+
+
+// Everything in javascript is an object
+
+
+
+let ArrowFun = (a, b) => {
+    console.log('Arrow Function', a, b);
+}
+
+ArrowFun(19, 80);
+
+
+let funNamevar = function () {
+    /*
+        code
+        return
+    */
+   /*
+        return
+   */
+
+        console.log('====inside solve ======')
+}
+
+funNamevar();
+
+console.log(funNamevar, typeof funNamevar)
+
+// function sum(a, b){
+//     console.log('--sum of a and b---',a+b);
+// }
+
+// console.log(sum(12,23));
+//console.log(sum1);
+
+
+// function print(){
+//     console.log(10);
+//     console.log(10);
+//     console.log(10);
+//     console.log(10);
+// }
+
+
+
+// print();
+
+
+
+/*
+
+
+
+
+
+*/
+
+
+
+
+
+// HomeWork 
+
+/*
+
+hoisting
+difference btwn function types
+
+
+
+next day
+callback
+*/
